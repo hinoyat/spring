a3c5a26c7c6ba2ef15566001b68da404a7c7d055
Hyunho Kang <hhwj2270@gmail.com>
2025-11-28 14:57:44 +0900

    init: bean 공부용 프로젝트 생성

---
+++ Bean And Spring Container/beantest/src/test/java/com/test/beantest/BeantestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.beantest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeantestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
