4f593bd74013c0589721ce650c79d2c274ef60ff
Hyunho Kang <hhwj2270@gmail.com>
2025-12-04 17:21:31 +0900

    feat: ApplicationContext 초기화 및 Bean 조회 확인용 Main 클래스 추가

--- Bean And Spring Container/spring-container-lifecycle/src/main/java/com/example/hinoyat/container/Main.java
+++ Bean And Spring Container/spring-container-lifecycle/src/main/java/com/example/hinoyat/container/Main.java
@@ -11,13 +11,21 @@ public class Main {
         ApplicationContext context =
             new AnnotationConfigApplicationContext(AppConfig.class);
         System.out.println("[Main] === 컨테이너 생성 완료 ===");
+        System.out.println();
 
         System.out.println("[Main] === MyBean 조회 ===");
         MyBean fromContext = context.getBean(MyBean.class);
         System.out.println("[Main] 조회 결과: " + fromContext);
-
         fromContext.hello();
+        System.out.println();
+
+        System.out.println("[Main] === AnotherBean 조회 ===");
+        AnotherBean anotherBean = context.getBean(AnotherBean.class);
+        anotherBean.useMyBean();
+        System.out.println();
+
+        System.out.println("[Main] === LazyBean 조회 ===");
+        LazyBean lazyBean = context.getBean(LazyBean.class);
+        lazyBean.doSomething();
     }
-
-
 }
